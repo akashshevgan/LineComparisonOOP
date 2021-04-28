bef4a5750bc562e263d56168274060d3f7805984
akash <akash@MSI>
2021-04-28 22:19:43 +0530

    Wellcome linecomparisonOOP

---
+++ LinecomparisonOOP.java
@@ -0,0 +1,10 @@
+package linecomparisonOOP;
+
+public class LinecomparisonOOP {
+	
+	public static void main (String[] args) {
+
+		System.out.println ("Welcome to Line Comparison using object oriented programing");
+
+	}
+}
